80a4113ab6891fb3408319602dd79f2b4fa9ff98
Ruslan <torokulruslan@gmail.com>
2025-02-19 11:57:38 +0600

    Русландын биринчи коммити

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const Header = () => {
     return(
         <header>
-            <h1>KAYAKKA</h1>
+            <h1>Каяка?</h1>
+            <a href=" О нас"></a>
+            <a href=" Главная"></a>
+            <a href=" Контакты"></a>
         </header>
     )
 }
