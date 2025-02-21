70510ba67ce5b5324632a06a0796af840b582ac1
diyorbek <artykovdierbek@gmail.com>
2025-02-21 16:40:27 +0600

    сайттын футери

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,51 @@
 import React from "react";
+import styles from './footer.module.css'
 
-const Footer = () => {
+const Footer = () => 
+{
     
 
     return(
-        <footer>
-            <h2>Diyor!!!!!!!!!!!!!!</h2>
+        <footer className={styles.footer}>
+            <div>
+                <div>
+                    <b>Abaut</b>
+                    <span>Abaut Karcis.com</span>
+                    <span>How it works</span>
+                    <span>Careers</span>
+                    <span>press</span>
+                    <span>Blog</span>
+                    <span>Forum</span>
+                <div>
+                    <div>
+                    <b>Partner with us</b>
+                    <span>Partnership programs</span>
+                    <span>Affiliate program</span>
+                    <span>Connectivity parthers</span>
+                    <span>Promotions and events</span>
+                    <span>Integrations</span>
+                    <span>Community</span>
+                    <span>Loyalty program</span>
+                    </div>
+                    <div>
+                    <b>Support</b>
+                    <span>Help Center</span>
+                    <span>Contact us</span>
+                    <span>privacy policy</span>
+                    <span>Terms of service</span>
+                    <span>Trust and safety</span>
+                    <span>Accessibility</span>
+                    </div>
+                    <div>
+                    <b>Get the app</b>
+                    <span>Karcis.com for Android</span>
+                    <span>Karics.com for iOS</span>
+                    <span>Mobile site</span>
+                    </div>
+
+                </div>
+
+            </div>
         </footer>
     )
 }
