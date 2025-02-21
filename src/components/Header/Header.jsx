179c04747586131354f37770e68407ac14eb9706
Ruslan <torokulruslan@gmail.com>
2025-02-21 16:36:51 +0600

    сайттын заголовогу

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,41 @@
 import React from "react";
+import styles from './header.module.css'
 
 const Header = () => {
     return(
-        <header>
-            <h1>Каяка?</h1>
-            <a href=" О нас"></a>
-            <a href=" Главная"></a>
-            <a href=" Контакты"></a>
+        <header className={styles.header}>
+
+           
+            <div className={styles.content}>
+
+                 <h1 className={styles.tytle}>Karsis.com </h1>
+
+            <nav>
+                
+                <a href="# ">Concerts </a>
+                <a href="# ">Arts </a>
+                <a href="# ">Conference </a>
+                <a href="# ">Movies </a>
+                <a href="# ">International </a>
+            </nav>
+            
+            <div className={styles.last}>
+                <nav>
+                <a href="#">
+              Log In
+             </a>
+             <a href="#">
+                Sign up
+             </a>
+             </nav>
+             
+
+
+            </div>
+
+            </div>
+
+
         </header>
     )
 }
