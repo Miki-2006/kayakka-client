e22f626ca08c2efb33ace504ca5c6e6abf97b813
Miki-2006 <rys5ek0v.111@gmail.com>
2025-03-05 12:11:57 +0600

    Добавлен глобальное хранилище

--- src/pages/Main/Categories/CategoriesOfEvents.jsx
+++ src/pages/Main/Categories/CategoriesOfEvents.jsx
@@ -1,20 +1,32 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import styles from "./categories.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchCategoriesSuccess,
+  fetchCategoriesStart,
+  fetchCategoriesFailure,
+} from "../../../features/categorySlice";
 
 const CategoriesOfEvents = () => {
-  const [categories, setCategories] = useState(null);
+  const dispatch = useDispatch();
+  const { categories } = useSelector((state) => state.categories);
 
   useEffect(() => {
     const getCategoriesFromServer = async () => {
-      const categories = await axios
-        .get("https://kayakka-server.vercel.app/api/category")
-        .then((res) => res);
-      setCategories(categories.data);
+      dispatch(fetchCategoriesStart());
+      try {
+        const categories = await axios
+          .get("https://kayakka-server.vercel.app/api/category")
+          .then((res) => res.data);
+        dispatch(fetchCategoriesSuccess(categories));
+      } catch (err) {
+        dispatch(fetchCategoriesFailure(err.message));
+      }
     };
 
     getCategoriesFromServer();
-  }, []);
+  }, [dispatch]);
   return (
     <ul className={styles.categories}>
       <li className={styles.category_active}>Все</li>
--- src/pages/Main/Events/Events.jsx
+++ src/pages/Main/Events/Events.jsx
@@ -1,24 +1,30 @@
 import axios from "axios";
 import styles from "./events.module.css";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Card from "../../../components/Card/Card"
+import { useDispatch, useSelector } from "react-redux";
+import { fetchEventsFailure, fetchEventsStart, fetchEventsSuccess } from "../../../features/eventSlice";
 
 const Events = () => {
-  const [events, setEvents] = useState(null);
+  const dispatch = useDispatch();
+  const { events } = useSelector((state) => state.events);
 
   useEffect(() => {
     const getAllEvents = async () => {
+      dispatch(fetchEventsStart())
       try {
-        await axios
+        const data = await axios
           .get("https://kayakka-server.vercel.app/api/events")
-          .then((res) => setEvents(res.data));
+          .then((res) => res.data);
+        dispatch(fetchEventsSuccess(data))
       } catch (err) {
         console.error("Ошибка при получении данных от сервера:", err);
+        dispatch(fetchEventsFailure(err.message))
       }
     };
 
     getAllEvents();
-  }, []);
+  }, [dispatch]);
   return (
     <div className={styles.events}>
       {
