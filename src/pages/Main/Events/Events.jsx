5fc7891c88b091b2bb77df58a438b60d6b8a57fb
Miki-2006 <rys5ek0v.111@gmail.com>
2025-03-07 15:22:09 +0600

    Добавил обработку категорий

--- src/pages/Main/Events/Events.jsx
+++ src/pages/Main/Events/Events.jsx
@@ -1,37 +1,39 @@
 import axios from "axios";
 import styles from "./events.module.css";
 import { useEffect } from "react";
-import Card from "../../../components/Card/Card"
+import Card from "../../../components/Card/Card";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchEventsFailure, fetchEventsStart, fetchEventsSuccess } from "../../../features/eventSlice";
+import {
+  fetchEventsFailure,
+  fetchEventsStart,
+  fetchEventsSuccess,
+} from "../../../features/eventSlice";
 
-const Events = () => {
+const Events = ({selectedCategory}) => {
   const dispatch = useDispatch();
   const { events } = useSelector((state) => state.events);
 
   useEffect(() => {
-    const getAllEvents = async () => {
-      dispatch(fetchEventsStart())
+    const getEventsByCategory = async () => {
+      dispatch(fetchEventsStart());
       try {
-        const data = await axios
-          .get("https://kayakka-server.vercel.app/api/events")
-          .then((res) => res.data);
-        dispatch(fetchEventsSuccess(data))
+        const url = selectedCategory
+          ? `https://kayakka-server.vercel.app/api/events/sort?category=${selectedCategory}` // запрос с категорией
+          : "https://kayakka-server.vercel.app/api/events"; // запрос без фильтрации
+        const data = await axios.get(url).then((res) => res.data);
+        dispatch(fetchEventsSuccess(data));
       } catch (err) {
         console.error("Ошибка при получении данных от сервера:", err);
-        dispatch(fetchEventsFailure(err.message))
+        dispatch(fetchEventsFailure(err.message));
       }
     };
 
-    getAllEvents();
-  }, [dispatch]);
+    getEventsByCategory();
+  }, [dispatch, selectedCategory]); // перезапускать запрос при изменении категории
+
   return (
     <div className={styles.events}>
-      {
-        events && events.map((el, indx) => (
-          <Card el={el} key={indx}/>
-        ))
-      }
+      {events && events.map((el, indx) => <Card el={el} key={indx} />)}
     </div>
   );
 };
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import CategoriesOfEvents from "./Categories/CategoriesOfEvents";
 import Events from "./Events/Events";
 import styles from "./main.module.css";
 
 const Main = () => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
   return (
     <main className={styles.main}>
       <div className={styles.container}>
-        <CategoriesOfEvents />
-        <Events />
+        <CategoriesOfEvents setSelectedCategory={setSelectedCategory}/>
+        <Events selectedCategory={selectedCategory}/>
       </div>
     </main>
   );
