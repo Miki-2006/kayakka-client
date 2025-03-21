c62a4bec96fdb60587fa9fdd674d24c3281bc3cf
Miki-2006 <rys5ek0v.111@gmail.com>
2025-03-21 12:05:26 +0600

    fghj

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Main from "./pages/Main/Main";
 import { ScaleLoader } from "react-spinners";
+import axios from "axios";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -14,13 +15,17 @@ function App() {
     }, 2000);
   }, []);
 
+  // useEffect(() => {
+  //   const getTokenOfUser = async () => {
+  //     try {
+  //       const token = await axios.get()
+  //     } catch (err) {
+        
+  //     }
+  //   }
+  // }, [])
 
-  if (loading)
-    return (
-      <div className="loader">
-        <ScaleLoader color="#5669ff" />
-      </div>
-    );
+  if (loading) return <div className="loader"><ScaleLoader color="#5669ff" /></div>
 
   return (
     <div className="App">
